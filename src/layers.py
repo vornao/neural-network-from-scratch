add losses class, fixed backprop bugs, speed impr.

src/layers.py
@@ -11,48 +11,38 @@ class Layer:
         self, units: int, input_shape: int, activation_function: af.Activation, bias=0.5
     ) -> None:
 
-        # init bias vector
-        self.bias = np.ones(units) * bias
-
-        # set output shape
+        # define shape of layer and number of units
         self.output_shape = units
         self.units = units
-
         self.activation = activation_function
 
-        # randomly init weight matrix
+        # randomly init weight matrix and biases
         self.W = np.random.uniform(size=(units, input_shape))
+        self.bias = np.ones(units) * bias
+
 
     def output(self, x):
         """
-        Compute layer's output
+        Compute layer's output and save it.
         """
-        self.lin_out = np.dot(self.W, x) + self.bias
-        out = self.activation.activation(self.lin_out)
-        self.last_input = x
-        self.last_out = out
-        return out
+        self.input = np.expand_dims(x, axis=1)
+        self.net = np.dot(self.W, x) + self.bias
+        self.out = self.activation.activation(self.net)
+        return self.out
 
-    def update_weights(self, deltas_prop, W_prop: np.ndarray, eta):
 
-        # save last weights to propagate
-        W_old = self.W
+    def update_weights(self, deltas, eta):
 
-        # compute deltas for each unit in layer.
-        # deltas is a vector with units length
-        deltas = np.multiply(
-            W_prop.T.dot(deltas_prop), 
-            self.activation.derivative(self.lin_out)
-        )
+        # compute dl to update weights and deltas to propagate back.
+        dl = np.multiply(deltas, self.activation.derivative(self.net))
+        deltas_prop = np.dot(dl, self.W)
+        delta_w = np.dot(np.expand_dims(dl, axis=1), self.input.T)
+        # update weights and biases
+        self.W = self.W - (eta * delta_w)
+        self.bias = self.bias - dl * eta
 
-        deltas = deltas.reshape(len(deltas), 1)  # TODO: how to not reshape everything?
-        last_inputs = self.last_input.reshape(len(self.last_input), 1)
-        self.W = self.W - eta * deltas.dot(last_inputs.T)
+        return deltas_prop
 
-        # update bias
-        self.bias = self.bias - deltas.flatten() * eta
-
-        return W_old, deltas.flatten()
 
     def __str__(self) -> str:
         return f"Weights matrix = {self.W} \n, biases = {self.bias}"
@@ -65,20 +55,18 @@ class OutputLayer(Layer):
 
         super().__init__(units, input_shape, activation_function, bias)
 
-    def update_weights(self, deltas_prop, W_prop, eta):
-        W_old = self.W
+    def update_weights(self, deltas, eta: int):
 
-        # compute deltas for output layer:
-        # in this case, we have the difference between labels and prediction
-        deltas = np.multiply(deltas_prop, self.activation.derivative(self.lin_out))
-        deltas = deltas.reshape(len(deltas), 1)
-        last_inputs = self.last_input.reshape(len(self.last_input), 1)
-        self.W = self.W - eta * deltas.dot(last_inputs.T)
+        # compute dl to update weights and deltas to propagate back.
+        dl = np.multiply(deltas, self.activation.derivative(self.net))
+        deltas_prop = np.dot(dl, self.W)
+        delta_w = np.dot(np.expand_dims(dl, axis=1), self.input.T)
 
-        # update bias
-        self.bias = self.bias - deltas.flatten() * eta
+        # update weights and biases
+        self.W = self.W - (eta * delta_w)
+        self.bias = self.bias - dl * eta
 
-        return W_old, deltas.flatten()
+        return deltas_prop
 
 
 class InputLayer(Layer):

src/main.py
@@ -9,40 +9,40 @@ from sklearn.metrics import roc_curve, roc_auc_score, accuracy_score
 from utils import one_hot_encoder, MultiClassError, SingleClassError
 
 
-
 def test_mnist():
-    mnist_train = pd.read_csv('data/mnist/train.csv')
+    mnist_train = pd.read_csv("data/mnist/train.csv")
 
-    y_train = one_hot_encoder(mnist_train[:30000]['label'].to_numpy())
-    x_train = mnist_train[:30000].drop(['label'], axis=1).to_numpy()
-    x_train = (x_train / 255)*.1  # type: ignore
+    y_train = one_hot_encoder(mnist_train[:30000]["label"].to_numpy())
+    x_train = mnist_train[:30000].drop(["label"], axis=1).to_numpy()
+    x_train = (x_train / 255) * 0.1  # type: ignore
 
-    y_valid = one_hot_encoder(mnist_train[30000:36000]['label'].to_numpy())
-    x_valid = mnist_train[30000:36000].drop(['label'], axis=1).to_numpy()
-    x_valid = (x_valid / 255)*.1  # type: ignore
+    y_valid = one_hot_encoder(mnist_train[30000:36000]["label"].to_numpy())
+    x_valid = mnist_train[30000:36000].drop(["label"], axis=1).to_numpy()
+    x_valid = (x_valid / 255) * 0.1  # type: ignore
 
-    y_test = one_hot_encoder(mnist_train[30000:36000]['label'].to_numpy())
-    x_test = mnist_train[30000:36000].drop(['label'], axis=1).to_numpy()
-    x_test = (x_test / 255)*.1  # type: ignore
+    y_test = one_hot_encoder(mnist_train[30000:36000]["label"].to_numpy())
+    x_test = mnist_train[30000:36000].drop(["label"], axis=1).to_numpy()
+    x_test = (x_test / 255) * 0.1  # type: ignore
 
-
-    net = network.Network(28*28)
+    net = network.Network(28 * 28)
     net.add_layer(10, ReLU())
     net.add_output_layer(10, Sigmoid(a=0.05))
-    net.train(x_train, y_train, x_valid, y_valid, MultiClassError(), epochs=10, eta=10e-2)
+    net.train(
+        x_train, y_train, x_valid, y_valid, MultiClassError(), epochs=10, eta=10e-2
+    )
 
     predictions = []
 
     for p in x_test:
         pred = net.output(p)
         predictions.append(np.round(pred))
-    
+
     predictions = np.array(predictions)
-    
-    accuracy = round(accuracy_score(y_test, predictions), 2)*100
+
+    accuracy = round(accuracy_score(y_test, predictions), 2) * 100
     roc = round(roc_auc_score(y_test, predictions), 2)
 
-    print(f'> Model accuracy: {accuracy}%. ROC AUC Score: {roc}')
+    print(f"> Model accuracy: {accuracy}%. ROC AUC Score: {roc}")
 
 
 def test_moons():
@@ -55,19 +55,21 @@ def test_moons():
     )
 
     x_val, x_test, y_val, y_test = train_test_split(
-        x_test_val, y_test_val, test_size = .25
+        x_test_val, y_test_val, test_size=0.25
     )
-
 
     net = network.Network(2)
     net.add_layer(8, ReLU())
     net.add_output_layer(1, Sigmoid(a=1))
 
     stats = net.train(
-        x_train, y_train, x_val, y_val, 
+        x_train,
+        y_train,
+        x_val,
+        y_val,
         estimator=SingleClassError(),
         eta=10e-2,
-        epochs=25
+        epochs=25,
     )
 
     predictions = []
@@ -75,12 +77,12 @@ def test_moons():
     for p in x_test:
         predictions.append(round(net.output(p)[0]))
 
-    predictions = np.array(predictions) 
+    predictions = np.array(predictions)
 
-    accuracy = round(accuracy_score(y_test, predictions), 2)*100
+    accuracy = round(accuracy_score(y_test, predictions), 2) * 100
     roc = round(roc_auc_score(y_test, predictions), 2)
 
-    print(f'> Model accuracy: {accuracy}%. ROC AUC Score: {roc}')
+    print(f"> Model accuracy: {accuracy}%. ROC AUC Score: {roc}")
 
     sns.lineplot(stats[0])
     sns.lineplot(stats[1])
@@ -91,6 +93,3 @@ if __name__ == "__main__":
 
     # test_moons()
     test_mnist()
-
- 
-    

src/network.py
@@ -2,7 +2,11 @@ import numpy as np
 from typing import List
 from utils import Error, TRAIN_FMT
 from layers import Layer, InputLayer, OutputLayer
+from losses import Loss
 from activation_functions import Activation
+from progress.bar import Bar
+
+import time
 
 
 class Network:
@@ -43,19 +47,29 @@ class Network:
         return out
 
     # TODO: remove pred-target cost evaluation to exploit minibatch training
-    def __backward_prop__(self, pred, target, eta=10e-3):
+    def __backward_prop__(self, pred, target, loss: Loss, eta=10e-3):
 
         """
         Perform backward propagation during training.
         """
         # compute difference among targets and actual prediction.
-        deltas_prop = np.add(pred, -target)
-        W_prop = np.zeros((1, 1))
+        # this in fact is the gradient
+        deltas = loss.dloss(pred, target)
+        # W_prop = np.zeros((1, 1))
 
         # now compute backward prop for every other layer, except for input layer.
         for l in reversed(self.layers[1 : len(self.layers)]):
-            W, deltas = l.update_weights(deltas_prop, W_prop, eta)
-            W_prop, deltas_prop = W, deltas
+            # TODO put only gradient :)
+            #
+            deltas = l.update_weights(deltas=deltas, eta=eta)
+
+    def multiple_outputs(self, patterns):
+        outputs = []
+
+        for p in patterns:
+            outputs.append(self.__forward_prop__(p))
+
+        return np.array(outputs)
 
     def output(self, x):
         """
@@ -63,24 +77,17 @@ class Network:
         """
         return self.__forward_prop__(x)
 
-    def compute_loss(self, x, y, estimator: Error):
-        error = 0
-        for x_t, y_t in zip(x, y):
-            pred = self.output(x_t)
-            error += estimator.validate(y_t, pred)
-
-        return error / len(x)
-
     def train(
         self,
         train_data,
         train_labels,
         val_data,
         val_labels,
-        estimator: Error,
+        metric: Error,
+        loss: Loss,
         epochs=25,
         eta=10e-3,
-        verbose=True
+        verbose=True,
     ):
         """
         Train network with given data and labels for requested epoch.
@@ -89,25 +96,42 @@ class Network:
         tr_stats = []
         val_stats = []
 
-        # TODO: 
+        # TODO:
         # - implement minibatch training computing error for b sized training labels and passing it to bacwkard prop function
         # - implement magnitude gradient descent algorithm
         for epoch in range(0, epochs):
 
-            for train, label in zip(train_data, train_labels):
-                self.__backward_prop__(
-                    pred= self.output(train), 
-                    target=label, 
-                    eta=eta
-                )
+            # Just some printing stuff
+            bar = Bar(f"Epoch: {epoch}/{epochs}", max=len(train_labels))
+            bar.bar_prefix = "["
+            bar.bar_suffix = "]"
 
-            tr_error = self.compute_loss(train_data, train_labels, estimator)
-            val_error = self.compute_loss(val_data, val_labels, estimator)
+            # Forward propagation over single pattern and back propagation
+            # for now it's just on-line learning.
+            # remove that ugly predictions list.
+
+            predictions = []
+
+            for train, label in zip(train_data, train_labels):
+                p = self.output(train)
+                predictions.append(p)
+
+                self.__backward_prop__(pred=p, target=label, eta=eta, loss=loss)
+
+                if verbose:
+                    bar.next()
+
+            tr_error = loss.loss(predictions, train_labels)
+            val_error = loss.loss(self.multiple_outputs(val_data), val_labels)
+
             if verbose:
                 print(TRAIN_FMT.format(epoch, round(tr_error, 4), round(val_error, 4)))
 
             tr_stats.append(tr_error)
             val_stats.append(val_error)
-            
+
+            if verbose:
+                bar.finish()
+
         print(TRAIN_FMT.format(epochs, round(tr_error, 4), round(val_error, 4)))
         return tr_stats, val_stats