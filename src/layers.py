08355e00746b2702809e535ec68cf8c4144914f9
g-lago8 <lagomarsinigiacomo@gmail.com>
2022-12-24 11:26:26 +0100

    Added nesterov momentum

--- src/layers.py
+++ src/layers.py
@@ -28,7 +28,7 @@ class Layer:
         self.last_input = np.NaN
         self.last_net = np.NaN
         self.last_output = np.NaN
-
+        self.last_delta = 0 # last delta used for momentum
     def net(self, x):
         return (self.W.T @ x) + self.bias
   
@@ -40,17 +40,18 @@ class Layer:
 
         return self.last_output
 
-    def update_weights(self, deltas, eta):
+    def update_weights(self, deltas, eta, nesterov):
         # compute dl to update weights and deltas to propagate back.
         dl = deltas * self.activation.derivative(self.last_net)
         delta_w = self.last_input @ dl.T
         deltas_prop = self.W @ dl
 
         if self.regularizer:
-            self.W -= eta * delta_w + self.regularizer.gradient(self.W)
+            self.W -= eta * delta_w + self.regularizer.gradient(self.W) + nesterov * self.last_delta
+            self.last_delta = eta * delta_w + self.regularizer.gradient(self.W)
         else:
-            self.W -= eta * delta_w
-
+            self.W -= eta * delta_w + nesterov * self.last_delta
+            self.last_delta = eta * delta_w
         self.bias -= dl * eta
 
         return deltas_prop
