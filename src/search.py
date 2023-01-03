dba30fd0465a778845d2747da5aa96477c0bd332
g-lago8 <lagomarsinigiacomo@gmail.com>
2023-01-03 18:05:53 +0100

    added  grid search with cross validation

--- src/search.py
+++ src/search.py
@@ -3,7 +3,7 @@ from src.losses import MeanSquaredError
 from src.metrics import BinaryAccuracy
 from itertools import product
 from heapq import nlargest, nsmallest
-
+from src.validation import kfold_cv
 
 def grid_search(
         model_shape: Network,
@@ -42,4 +42,40 @@ def grid_search(
         best_parameters = [param[i] for i in nsmallest(k, param)]
     else:
         best_parameters = [param[i] for i in nlargest(k, param)]
+    return best_parameters
+
+
+def grid_search_cv(
+        model_shape: Network,
+        x,
+        y,
+        number_par=1,
+        metric=BinaryAccuracy(),
+        loss=MeanSquaredError(),
+        eta=(1e-3,),
+        nesterov=(0.5,),
+        reg_type=(None,),
+        reg_val=(1e-10,),
+        metric_decreasing=0,
+        epochs=100
+):
+    parameters = product(eta, nesterov, reg_type, reg_val)
+    param = {}  # dictionary accuracy : set of parameters
+    for par in parameters:
+        [eta, nesterov, reg_type, reg_val] = par
+        if reg_type is not None:
+            model = Network(model_shape.layers[0].units, reg_type(reg_val))
+        else:
+            model = Network(model_shape.layers[0].units)
+        for layer in model_shape.layers[1:]:
+            model.add_layer(layer.units, layer.activation)
+
+        print("eta=", eta, "nesterov=", nesterov, "reg ", reg_type, "lambda =", reg_val)
+        acc = kfold_cv(model=model, x=x, y=y, k=5,  eta=eta, nesterov=nesterov, verbose=True, epochs=epochs
+                       , metric=metric, loss=loss)
+        param.update({acc: par})
+    if metric_decreasing:
+        best_parameters = [param[i] for i in nsmallest(number_par, param)]
+    else:
+        best_parameters = [param[i] for i in nlargest(number_par, param)]
     return best_parameters
--- test/gs_test.py
+++ test/gs_test.py
@@ -1,47 +1,29 @@
-from src.search import grid_search
+from src.search import grid_search, grid_search_cv
 import numpy as np
-from src.utils import load_moons,  load_cup
+from src.utils import load_moons, load_cup
 from src.network import Network
 from src.activations import ReLU, Tanh, Sigmoid
 from src.losses import MeanSquaredError
-from src.metrics import BinaryAccuracy, MulticlassAccuracy, MeanEuclideanError
+from src.metrics import BinaryAccuracy,  MeanEuclideanError
 
 from sklearn.metrics import accuracy_score
 
-from src.regularizers import L2, L1
-
-
-
 x_train, x_val, y_train, y_val = load_cup()
+x = np.append(x_train, x_val, axis=0)
+y = np.append(y_train, y_val, axis=0)
+
 model = Network(9)
 model.add_layer(16, ReLU())
 model.add_layer(8, ReLU())
 model.add_layer(2, Sigmoid())
+print(x_train.shape)
+print(x_val.shape)
+print(x.shape)
 
-best_par = grid_search(model, x_train, y_train, x_val, y_val,
-                       metric=MeanEuclideanError(), loss=MeanSquaredError(), epochs=200,
-                       eta=[1e-1, 1e-2, 1e-3, 1e-4], nesterov=[0.5, 0.8], reg_type=(None,), reg_val=(0,), metric_decreasing=1)
+best_par = grid_search_cv(model_shape=model, x=x, y=y,
+                          metric=MeanEuclideanError(), loss=MeanSquaredError(), epochs=10,
+                          eta=[1e-1, 1e-2, 1e-3, 1e-4], nesterov=[0.5, 0.8], reg_type=(None,), reg_val=(0,),
+                          metric_decreasing=1)
 
 print(best_par)
 
-
-x_train, x_val, x_test, y_train, y_val, y_test = load_moons(validation=True, noise=0.2)
-model = Network(2)
-model.add_layer(8, ReLU())
-model.add_layer(1, Tanh())
-best_par = grid_search(model, x_train, y_train, x_val, y_val,
-                       eta=(1e-1, 1e-2, 1e-3), nesterov=(0.5, 0.8), reg_type=(None,), reg_val=(0,))
-print(best_par)
-
-for par in best_par:
-    eta, nesterov, regularizer, lamda = par
-    model = Network(2)
-    model.add_layer(8, ReLU())
-    model.add_layer(1, Tanh())
-    model.train(train=(x_train, y_train), validation=(x_val, y_val),
-                eta=eta, nesterov=nesterov, epochs=200, verbose=True,
-                loss=MeanSquaredError(), metric=BinaryAccuracy())
-
-    predictions = model.multiple_outputs(x_test)
-
-    print(accuracy_score(y_test.flatten(), np.round(predictions.flatten())))
