cd0a779dd86b7e103e54a2989dd3ea430f7b33d6
Leonardo Stoppani <58340900+lilf4p@users.noreply.github.com>
2023-01-21 18:59:54 +0100

    fix monk test

--- src/utils.py
+++ src/utils.py
@@ -71,7 +71,7 @@ def load_monk1(test_size=0.2):
 
     # train validation test split
     X_train, X_val, y_train, y_val = train_test_split(
-        X_train, y_train, test_size=test_size, random_state=42
+        X_train, y_train, test_size=test_size, random_state=12
     )
 
     return X_train, X_val, X_test, y_train, y_val, y_test
